72e527ed4311002b231fd180e8c6e0c9a6730dd7
Vlad Danciu <vdanciu@gmail.com>
2017-12-28 16:38:44 +0200

    more refactoring

--- index.js
+++ index.js
@@ -28,41 +28,50 @@ app.post('/drain/' + token, function (req, res) {
 
     const line = req.logLine;
     let metric = null;
-    if (line.includes('heroku router') && line.includes('service=') && line.includes('status=')) {
+    if (line.includes('heroku router') &&
+        line.includes('service=') &&
+        line.includes('status=')) {
         // 2016-06-09T07:32:15.360608+00:00 app[web.1]: 239 <158>1 2016-06-09T07:32:14.500838+00:00 host heroku router - at=info method=POST path="/login" host=app.dougs.fr request_id=7f4e29d2-e2f1-42d0-b47b-9150b2b1183a fwd="46.246.28.90" dyno=web.2 connect=1ms service=129ms status=200 bytes=5016
-        metric = getMetricForRouter(line)
-    } else if (line.includes('heroku web') && line.includes('source=') && line.includes('memory_total=')) {
+        metric = getMetricForRouter(line);
+    } else if (line.includes('heroku web') &&
+        line.includes('source=') &&
+        line.includes('memory_total=')) {
         // 2016-06-09T07:32:16.527056+00:00 app[web.1]: 339 <45>1 2016-06-09T07:32:16.163266+00:00 host heroku web.2 - source=web.2 dyno=heroku.32934028.9646d93e-977a-421a-a5ef-02adc583e5b5 sample#memory_total=247.22MB sample#memory_rss=204.91MB sample#memory_cache=41.48MB sample#memory_swap=0.82MB sample#memory_pgpgin=2377742pages sample#memory_pgpgout=2339192pages sample#memory_quota=1024.00MB
-        metric = getMetricForWeb(line)
-    } else if (line.includes('heroku-postgres') && line.includes('source=') && line.includes('load-avg-15m=')) {
+        metric = getMetricForWeb(line);
+    } else if (line.includes('heroku-postgres') &&
+        line.includes('source=') &&
+        line.includes('load-avg-15m=')) {
         // 2016-06-09T07:53:07.315320+00:00 app[web.1]: 531 <134>1 2016-06-09T07:52:53+00:00 host app heroku-postgres - source=DATABASE sample#current_transaction=48441 sample#db_size=247529644.0bytes sample#tables=28 sample#active-connections=3 sample#waiting-connections=0 sample#index-cache-hit-rate=0.99991 sample#table-cache-hit-rate=0.99994 sample#load-avg-1m=0.055 sample#load-avg-5m=0.035 sample#load-avg-15m=0.025 sample#read-iops=0 sample#write-iops=0.29701 sample#memory-total=3786332.0kB sample#memory-free=531500kB sample#memory-cached=2685020.0kB sample#memory-postgres=52128kB
-        return getMetricForPostgres(line)
+        metric = getMetricForPostgres(line);
     }
 
     saveMetric(metric)
 });
 
-app.get('/service/' + token, function (req, res) {
-    const query = `
-    WITH intervals AS (
-        SELECT
-          i AS                                                              id,
-          (now() AT TIME ZONE 'utc') - ((i + 10) || ' minutes') :: INTERVAL start_time,
-          (now() AT TIME ZONE 'utc') - ((i) || ' minutes') :: INTERVAL        end_time
-        FROM generate_series(0, 120, 10) AS i
-    )
+function getQueryForService(type) {
+    return `WITH intervals AS (
+                SELECT
+                i AS  id,
+                (now() AT TIME ZONE 'utc') - ((i + 10) || ' minutes') :: INTERVAL start_time,
+                (now() AT TIME ZONE 'utc') - ((i) || ' minutes') :: INTERVAL        end_time
+                FROM generate_series(0, 120, 10) AS i
+            )
 
-    SELECT
-    count(metrics.id),
-        avg(metrics.service) AS average,
-        intervals.start_time,
-        intervals.end_time
-    FROM intervals
-    INNER JOIN metrics ON metrics.date >= intervals.start_time AND metrics.date < intervals.end_time
-    WHERE
-    metrics.type = 'router'
-    GROUP BY intervals.id, intervals.start_time, intervals.end_time
-    ORDER BY intervals.id DESC;`;
+            SELECT
+            count(metrics.id),
+                avg(metrics.service) AS average,
+                intervals.start_time,
+                intervals.end_time
+            FROM intervals
+            INNER JOIN metrics ON metrics.date >= intervals.start_time AND metrics.date < intervals.end_time
+            WHERE
+            metrics.type = '${type}'
+            GROUP BY intervals.id, intervals.start_time, intervals.end_time
+            ORDER BY intervals.id DESC;`;
+}
+
+app.get('/service/' + token, function (req, res) {
+    query = getQueryForService('router');
 
     pgQuery(query, (err, result) => {
         if (err) res.sendStatus(500);
@@ -80,26 +89,7 @@ app.get('/service/' + token, function (req, res) {
 });
 
 app.get('/throughput/' + token, function (req, res) {
-    const query = `
-    WITH intervals AS (
-        SELECT
-          i AS                                                              id,
-          (now() AT TIME ZONE 'utc') - ((i + 10) || ' minutes') :: INTERVAL start_time,
-          (now() AT TIME ZONE 'utc') - ((i) || ' minutes') :: INTERVAL        end_time
-        FROM generate_series(0, 120, 10) AS i
-    )
-
-    SELECT
-    count(metrics.id),
-        avg(metrics.service) AS average,
-        intervals.start_time,
-        intervals.end_time
-    FROM intervals
-    INNER JOIN metrics ON metrics.date >= intervals.start_time AND metrics.date < intervals.end_time
-    WHERE
-    metrics.type = 'router'
-    GROUP BY intervals.id, intervals.start_time, intervals.end_time
-    ORDER BY intervals.id DESC;`;
+    const query = getQueryForService('router');
 
     pgQuery(query, (err, result) => {
         if (err) res.sendStatus(500);
